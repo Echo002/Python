625f5ee5d25b16217af9f0c802cbbe7f9277fe2e
Dylan <xugaohero@163.com>
2018-09-13 10:59:41 +0800

    update

---
+++ 1007 素数对猜想.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+n = int(input())
+num=[]
+count = 0
+for i in range(2,n):
+    j = 2
+    for j in range(2, i):
+        if (i % j == 0):
+            break
+    else:
+        num.append(i)
+for k in range(1,len(num)):
+    if(num[k] - num[k-1] == 2):
+        count += 1
+print(count)
