c78b7b54d43126d9cd0ce0c5d1ecf81ce956dec3
Echo002 <xugaohero@163.com>
2018-09-16 18:05:09 +0800

    update

--- PTA/01 Basic/1008 数组元素循环右移问题.py
+++ PTA/01 Basic/1008 数组元素循环右移问题.py
@@ -23,17 +23,21 @@
 #            └──┴──┘       └──┴──┘
 #                神兽保佑
 #                BUG是不可能有BUG的!
-import math
-n = int(input())
-num=[]
-count = 0
-for i in range(2,n):
-    for j in range(2, int(math.sqrt(i)) + 1):
-        if i % j == 0:
-            break
-    else:
-        num.append(i)
-for k in range(1,len(num)):
-    if num[k] - num[k-1] == 2:
-        count += 1
-print(count)
+str1 = input()
+str2 = input()
+deal_1 = str1.split()
+deal_e = str2.split()
+deal_2 = []
+str = ''
+num = int(deal_1[0])
+step = int(deal_1[1])
+for i in deal_e:
+    deal_2.append(int(i))
+for i in range(step):
+    temp = deal_2[-1]
+    for j in range(0,num-1):
+        deal_2[num - 1 - j] = deal_2[num -2 -j]
+    deal_2[0] = temp
+#print(deal_2)
+result = [str(x) for x in deal_2]
+print(result)
