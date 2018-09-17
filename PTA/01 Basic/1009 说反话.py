d6f9fbda32d2d114df809365d7f00db64a3d9105
Dylan <xugaohero@163.com>
2018-09-17 10:59:19 +0800

    update

--- PTA/01 Basic/1009 说反话.py
+++ PTA/01 Basic/1009 说反话.py
@@ -23,21 +23,10 @@
 #            └──┴──┘       └──┴──┘
 #                神兽保佑
 #                BUG是不可能有BUG的!
-str1 = input()
-str2 = input()
-deal_1 = str1.split()
-deal_e = str2.split()
-deal_2 = []
-num = int(deal_1[0])
-step = int(deal_1[1])
-for i in deal_e:
-    deal_2.append(int(i))
-for i in range(step):
-    temp = deal_2[-1]
-    for j in range(0,num-1):
-        deal_2[num - 1 - j] = deal_2[num -2 -j]
-    deal_2[0] = temp
-for i in range(len(deal_2)):
-    deal_2[i] = str(deal_2[i])
-#result = [str(x) for x in deal_2]
-print(' '.join(deal_2))
+str = input()
+result = []
+deal = str.split()
+#print(deal)
+for i in range(len(deal)):
+    result.append(deal[len(deal) - 1 - i])
+print(" ".join(result))
--- PTA/01 Basic/1010 一元多项式求导.py
+++ PTA/01 Basic/1010 一元多项式求导.py
@@ -23,21 +23,25 @@
 #            └──┴──┘       └──┴──┘
 #                神兽保佑
 #                BUG是不可能有BUG的!
-str1 = input()
-str2 = input()
-deal_1 = str1.split()
-deal_e = str2.split()
-deal_2 = []
-num = int(deal_1[0])
-step = int(deal_1[1])
-for i in deal_e:
-    deal_2.append(int(i))
-for i in range(step):
-    temp = deal_2[-1]
-    for j in range(0,num-1):
-        deal_2[num - 1 - j] = deal_2[num -2 -j]
-    deal_2[0] = temp
-for i in range(len(deal_2)):
-    deal_2[i] = str(deal_2[i])
-#result = [str(x) for x in deal_2]
-print(' '.join(deal_2))
+num = []
+deal = input().split()
+for i in deal:
+    num.append(int(i))
+result = []
+if len(num) == 2 and num[1] == 0:
+    result.extend(['0', '0'])
+else:
+    for i in range(0, len(num) - 1, 2):
+        x = num[i] * num[i+1]
+        z = num[i+1] - 1
+        if z < 0:
+            # result.extend(['0', '0'])
+            continue
+        if x == 0:
+            result.extend(['0', '0'])
+            continue
+        result.append(x)
+        result.append(z)
+for i in range(len(result)):
+    result[i] = str(result[i])
+print(" ".join(result))
