dd4736461ef2a9e8006bb1dc238a18daf319d34f
Dylan <xugaohero@163.com>
2018-09-18 18:54:56 +0800

    update

--- PTA/01 Basic/1012 数字分类.py
+++ PTA/01 Basic/1012 数字分类.py
@@ -23,18 +23,3 @@
 #            └──┴──┘       └──┴──┘
 #                神兽保佑
 #                BUG是不可能有BUG的!
-deal = []
-count = 1
-num = int(input())
-for i in range(num):
-    list = input().split()
-    for j in range(len(list)):
-        list[j] = float(list[j])
-    deal.append(list)
-for i in deal:
-    if i[0] + i[1] > i[2]:
-        print("Case #"+ str(count) + ": true")
-    else:
-        print("Case #"+ str(count) + ": false")
-    count += 1
-
