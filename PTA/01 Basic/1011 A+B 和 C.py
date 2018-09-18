0b82a4bf7e112121183ded931763a7d0a49a949b
Echo002 <xugaohero@163.com>
2018-09-18 16:58:01 +0800

    update

---
+++ PTA/01 Basic/1011 A+B 和 C.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+deal = []
+count = 1
+num = int(input())
+for i in range(num):
+    list = input().split()
+    for i in range(len(list)):
+        list[i] = int(list[i])
+    deal.append(list)
+for i in deal:
+    if i[2] > i[0] + i[1]:
+        print("Case #"+ str(count) + ": true")
+    else:
+        print("Case #"+ str(count) + ": false")
+    count += 1
+
