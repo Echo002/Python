d7e8e3b83cd9505ae6eddaf783350d14b6943a1e
Echo002 <xugaohero@163.com>
2018-09-20 16:48:33 +0800

    update

---
+++ LeetCode/01 Basic/28 实现strStr().py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        print(needle[0])
+        if 'l' in haystack:
+            print("ok")
+
+# 以下是测试代码：
+s1 = "hello"
+s2 = "ll"
+s1 = Solution()
+s = s1.strStr(s1,s2)
+print(s)
