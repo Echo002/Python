0b3527a5c6ef6e99af3d561977521162523001c1
Echo002 <xugaohero@163.com>
2018-09-29 21:31:12 +0800

    update

---
+++ LeetCode/01 Basic/21 合并两个有序链表.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
---
+++ PTA/01 Basic/1015 德才论(未解决).py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#!-*-coding:utf-8 -*-
+#!@Author:xugao
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │                 │
+#      │    ＞  　　＜    │
+#      │                 │
+#      │  ....　⌒　....　│
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │                        ├─┐
+#          │                        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+#                神兽保佑
+#                BUG是不可能有BUG的!
+
+str = input().split()
+index = []
+student = []
+result = []
+for i in str:
+    index.append(int(i))
+for j in range(index[0]):
+    str = input().split()
+    demo = []
+    for k in str:
+        demo.append(int(k))
+    student.append(demo)
+print(student)
+print(sorted(student,key=lambda x:x[2],reverse=True))
+print(sorted(student,key=lambda x:(x[2],x[1]),reverse=True))
