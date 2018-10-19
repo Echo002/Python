5c2a699154e10eed26354a0000ad1260f601fdfd
Echo002 <xugaohero@163.com>
2018-10-19 22:50:11 +0800

    update

--- LeetCode/01 Basic/168 Excel表格名称.py
+++ LeetCode/01 Basic/168 Excel表格名称.py
@@ -32,13 +32,14 @@ class Solution:
         dic = {1:'A', 2:'B', 3:'C',4:'D',5:'E',6:'E',7:'G',8:'H',
                9: 'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',
                17: 'Q',18:'R',19:'S',20:'T',21:'U',22:'v',23:'w',24:'x',
-               25: 'Y',26:'Z',}
+               25: 'Y',0:'Z',}
         count = 0
         result = []
-        while(n >= 0):
+        while(n != 0):
             r = n % 26
             result.insert(0,dic[r])
             n -= r * pow(26, count)
+            count += 1
         return result
 
 # 以下是测试代码：
