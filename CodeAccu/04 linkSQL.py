f37cbf0c34d570e2dd200f2fd19f107ef7e48b42
Dylan <xugaohero@163.com>
2018-11-08 12:10:29 +0800

    update

--- CodeAccu/04 linkSQL.py
+++ CodeAccu/04 linkSQL.py
@@ -27,19 +27,12 @@ import pymysql
 import codecs
 import csv
 
-# 使用 cursor() 方法创建一个游标对象 cursor
-
-# with open('users.csv','a+') as fw:
-#     for i in range(len(data)):
-#         fw.write("".join(tuple(data[i])))
-
 def red_mysql_to_csv(filename):
-    with codecs.open(filename=filename,mode='w',encoding='utf-8')as f:
-        write = csv.writer(f,dialect='excel')
+    with codecs.open(filename = filename, mode = 'a+',encoding = 'utf-8')as f:
+        write = csv.writer(f,dialect = 'excel')
         write.writerow(['车辆标识','触发事件','运营状态','GPS时间','GPS经度','GPS纬度','GPS速度','GPS方位','GPS状态'])
         db = pymysql.connect("localhost", "root", "root", "Demo")
-        cursor = db.cursor()
-        sql =r"select * from gps1 where 触发事件 =1 and GPS状态=1 and GPS方位>300 and GPS速度 > 40;"
+        sql =r"select GPS经度,GPS纬度 from gps1 where 触发事件 = 2 and 运营状态 = 2 and GPS速度 > 0 and GPS方位 > 300 and 车辆标识=162411;"
         cursor = db.cursor()
         cursor.execute(sql)
         results = cursor.fetchall()
--- test.py
+++ test.py
@@ -1,37 +1,18 @@
-#!/usr/bin/env python
-#!-*-coding:utf-8 -*-
-#!@Author:xugao
-#         ┌─┐       ┌─┐
-#      ┌──┘ ┴───────┘ ┴──┐
-#      │                 │
-#      │                 │
-#      │    ＞  　　＜    │
-#      │                 │
-#      │  ....　⌒　....　│
-#      │                 │
-#      └───┐         ┌───┘
-#          │         │
-#          │         │
-#          │         │
-#          │         └──────────────┐
-#          │                        │
-#          │                        ├─┐
-#          │                        ┌─┘
-#          │                        │
-#          └─┐  ┐  ┌───────┬──┐  ┌──┘
-#            │ ─┤ ─┤       │ ─┤ ─┤
-#            └──┴──┘       └──┴──┘
-#                神兽保佑
-#                BUG是不可能有BUG的!
+import pymysql
+import codecs
+import csv
 
+def red_mysql_to_csv(filename):
+    with codecs.open(filename = filename, mode = 'a+',encoding = 'utf-8')as f:
+        write = csv.writer(f,dialect = 'excel')
+        db = pymysql.connect("localhost", "root", "root", "zhanghao")
+        sql = r"select * from gps1 where  事件 = 4 and 时间<20121101001700 and 状态 > 0 and 速度 =0  and 方位=290  ;"
 
-'''
-17623223623
-62468319
-测试代码
-微博登录:17169884113
-密码aaa456123
-'''
+        cursor = db.cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for res in results:
+            print(res)
+            write.writerow(res)
 
-for i in range(len(data)):
-    print(data[i])
+red_mysql_to_csv('result.csv')
