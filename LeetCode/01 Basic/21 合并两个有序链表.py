9fef974bc015ffea18b054225d1ddd59ca229665
Echo002 <xugaohero@163.com>
2018-11-12 18:56:21 +0800

    update

--- LeetCode/01 Basic/21 合并两个有序链表.py
+++ LeetCode/01 Basic/21 合并两个有序链表.py
@@ -24,11 +24,17 @@
 #                神兽保佑
 #                BUG是不可能有BUG的!
 
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+    def travel(self):
+        cur = self
+        while cur != None:
+            print(cur.val, end=" ")
+            cur = cur.next
+        print("\n", end='')
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
@@ -37,3 +43,45 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
+        res = ListNode(0)
+        first = res
+        cur1 = l1
+        cur2 = l2
+        while cur1 != None or cur2 != None:
+            if cur1 == None:
+                res.next = cur2
+                return first.next
+            if cur2 == None:
+                res.next = cur1
+                return first.next
+            if cur1.val > cur2.val:
+                res.next = cur2
+                cur2 = cur2.next
+            else:
+                res.next = cur1
+                cur1 = cur1.next
+            res = res.next
+
+# 以下是测试代码：
+
+
+l1 = ListNode(2)
+n1 = ListNode(3)
+n2 = ListNode(4)
+n3 = ListNode(9)
+l1.next = n1
+n1.next = n2
+n2.next = n3
+
+l2 = ListNode(3)
+m1 = ListNode(5)
+m2 = ListNode(7)
+m3 = ListNode(8)
+l2.next = m1
+m1.next = m2
+m2.next = m3
+
+s1 = Solution()
+s = s1.mergeTwoLists(l1, l2)
+print(l1,l2)
+print(s.travel())
--- test.py
+++ test.py
@@ -1,17 +1,4 @@
-import pymysql
-import codecs
-import csv
-
-def red_mysql_to_csv(filename):
-    with codecs.open(filename = filename, mode = 'a+',encoding = 'utf-8')as f:
-        write = csv.writer(f,dialect = 'excel')
-        db = pymysql.connect("localhost", "root", "root", "zhanghao")
-        sql = r"select * from gps1 where  事件 = 4 and 时间<20121101001700 and 状态 > 0 and 速度 =0  and 方位=290  ;"
-
-        cursor = db.cursor()
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        for res in results:
-            print(res)
-            write.writerow(res)
-red_mysql_to_csv('result.csv')
+a = 3
+b = 4
+a, b = b, a
+print(a, b)
